4cf792ae6088555690fa4991b80973fef8680c0d
Ishita Jaiswal <108209264+Ishita8888@users.noreply.github.com>
2023-03-16 12:30:47 +0530

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react'
+// import { useContext } from 'react';
+// import noteContext from '../context/notes/noteContext';
+const About = () => {
+  
+  
+  return (
+    <div>About PAge
+    </div>
+  )
+}
+
+export default About;
